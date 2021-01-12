1398e784db70fdb846918bc977dfb21648241d88
hossainchisty <hossain.chisty11@gmail.com>
2021-01-12 16:49:27 +0600

    Solved

---
+++ Day 3 Intro to Conditional Statements.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    N = int(input())
+
+
+if N % 2 != 0:
+    print("Weird")
+else:
+    if N <= 5:
+        print("Not Weird")
+    elif N <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
